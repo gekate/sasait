77263e6e2460928248e9282c2b55afd0739be531
gekate <97360207+gekate@users.noreply.github.com>
2023-03-29 19:05:06 +0400

    Delete Проэкт directory
    тупанул

--- Проэкт/script.js
+++
@@ -1,9 +0,0 @@
-function handle() {
-    let inputs = document.querySelectorAll("input");
-    let input_values = [];
-    for (let i = 0; i < inputs.length; i++) {
-        input_values.push(inputs[i].value);
-    }
-}
-let button = document.querySelector('.btn');
-button.addEventListener('click', handle);
